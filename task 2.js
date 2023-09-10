5af0382ce77cf85742899c8c21a3a29d8c90e90b
pavithra <130351122+pavithra-k20cs29@users.noreply.github.com>
2023-09-10 09:48:54 +0530

    Add files via upload

---
+++ task 2.js
@@ -0,0 +1,7 @@
+function lion(){
+    var a="Pavithra";
+    var b="Sarathy";
+    var c="20";
+    console.log("FirstName="+a+" "+"LastName="+b+" "+"Age="+c);
+}
+lion();
---
+++ task 3.js
@@ -0,0 +1,12 @@
+
+function Year(){
+    var y1=2003;
+    var x1=2003;
+    var y2=2025;
+    var x2=2026;
+    var z1=y2-y1;
+    var z2=x2-x1;
+    var word
+    console.log("I will be either"+" "+z1+" "+"or"+" "+z2+" "+"in"+" "+x2);
+}
+Year();
