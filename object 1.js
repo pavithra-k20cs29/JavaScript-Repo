2adc68b4e4a4fce676f0afeded1c872815b92573
pavithra <130351122+pavithra-k20cs29@users.noreply.github.com>
2023-09-23 14:13:34 +0530

    Add files via upload

---
+++ object 1.js
@@ -0,0 +1,5 @@
+let vehicle = {
+    name : "Mercedes Benz" , model : "C - Class",manufacture :" Daimler AG Company",length : "4686 mm",cargocapacity : " 12.6 cubic feet" 
+}
+console.log(vehicle)
+
---
+++ object 2.js
@@ -0,0 +1,5 @@
+let movie=
+{
+    title : 'Bahubali', directors : 'Rajamouli' , producers : ['Sobhu','Raghavendra','Prasad'], 'lead actors with their roles' : { Prabhas : 'Baahubali' , anushka : 'Devasena', rana : 'Bhallaladeva' , tamannah : 'Avanthika'}
+}
+console.log(movie)
