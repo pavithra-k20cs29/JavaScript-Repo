cab4a67f3207facd1ace86232426a74878e8f58e
pavithra <130351122+pavithra-k20cs29@users.noreply.github.com>
2023-09-28 10:18:34 +0530

    Add files via upload

---
+++ math 2.js
@@ -0,0 +1,6 @@
+        function decimals(n, d) {
+            return n.toFixed(d);
+        }
+        console.log(decimals(2.100212, 2));
+        console.log(decimals(2.100212, 3));
+        console.log(decimals(2100, 2));
